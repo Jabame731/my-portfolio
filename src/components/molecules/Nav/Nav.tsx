ca73ba89b9c263d89aedd11def9d97f8e7d80ff8
Jabame731 <justinetaglucop@gmail.com>
2025-09-18 01:04:36 +0800

    change

--- src/components/molecules/Nav/Nav.tsx
+++ src/components/molecules/Nav/Nav.tsx
@@ -3,10 +3,10 @@ import MobileNav from '../MobileNav/MobileNav';
 
 const Nav = () => {
   const navLinks: NavLinkModel[] = [
-    { label: 'home', location: '#home' },
-    { label: 'expertise', location: '#expertise' },
-    { label: 'projects', location: '#projects' },
-    { label: 'about me', location: '#about-me' },
+    { label: 'Home', location: '#home' },
+    { label: 'Tech Stack', location: '#expertise' },
+    { label: 'Projects', location: '#projects' },
+    { label: 'About Me', location: '#about-me' },
   ];
   return (
     <>
